b0e17fad8eb8281aec1ae2bc5b3284b853700538
Marion <marion.carambula@modop.com>
2018-07-13 18:15:15 -0500

    Starting logic for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import Logo from '../Logo/Logo';
 import './Login.css';
@@ -7,32 +7,46 @@ class Login extends Component {
     state = {
         user: ''
     };
+    renderUsers(userArray, users) {
+        return (
+            <Fragment>
+                <option value=''>Who are you?</option>
+                {
+                    userArray.map( id => (
+                        <option key={id} value={id}>
+                            {users[id].name}
+                        </option>
+                    ))
+                }
+            </Fragment>
+        )
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.user);
+        //// TODO:  go to dashboard
+    }
     render() {
         const { users, loading } = this.props;
         const userArray = users ? Object.keys(users) : [];
         return (
             <div className='login'>
-                <div className='login-box'>
+                <form className='login-box' onSubmit={this.handleSubmit}>
                     <div className='sign-in'>Sign In</div>
                     <Logo />
                     <div className='inner-box'>
-                        User:
-                        <select>
-                            {
-                            !loading ? (
-                                    userArray.map( id => (
-                                        <option key={id} value={id}>
-                                            {users[id].name}
-                                        </option>
-                                    ))
-                                )
-                            :
+                        <select value={this.state.user} onChange={(e)=> this.setState({user: e.target.value})}>
+                        {
+                            (!loading)
+                                ?
+                                this.renderUsers(userArray, users)
+                                :
                                 <option>Loading... </option>
-                            }
+                        }
                         </select>
-                        <button>SUBMIT</button>
+                        <button disabled={this.state.user != '' ? false: true}>SUBMIT</button>
                     </div>
-                </div>
+                </form>
             </div>
         )
     }
