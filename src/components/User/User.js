c34c4de188368ca99820f062785e9b30ceaff43d
Marion <marion.carambula@modop.com>
2018-08-02 18:34:44 -0500

    feat

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,10 +5,18 @@ import './User.css';
 const User = ({ user, showInNav }) => {
     const classInLine = (showInNav) ? 'user-nav': 'user';
     return (
-            <div className={`${classInLine}`}>
-                <img src={user.avatarURL}  alt={user.name} />
-                {user.name}
+            <form onSubmit={this.handleSubmit}>
+            <div className='options'>
+                <input
+                    name='name'
+                    type='text'
+                    value={optionOne}
+                    placeholder='Name'
+                    onChange={(e) => this.handleOptionChange(e, OPTION_ONE)}
+                />
             </div>
+        <button className='button'>Create</button>
+        </form>
     )
 }
 
